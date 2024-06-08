59bc566c61de929a826ba25b4cce369ba15f47e8
rezafikkri <fikkri.reza@gmail.com>
2024-06-08 20:47:07 +0700

    feat: and check activation-key from database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,47 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const supabase = require('./database');
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+// parse various different custom JSON types as JSON
+app.use(bodyParser.json({ type: 'application/*+json' }))
+
+app.get('/', async (req, res) => {
+  return res.status(200).json({
+    status: true,
+    message: 'This is api for tidyfiles app activation',
+  });
+});
+
+app.post('/api/check/activate', async (req, res) => {
+  try {
+    const activationKey = req.body.activationKey;
+    // verify activation key
+    jwt.verify(activationKey, process.env.ACTIVATION_SECRET);
+    // check activation key exists in database (using prisma)
+    const { data, error } = await supabase
+      .from('activation_key')
+      .select('key')
+      .eq('key', activationKey);
+
+    if (data.length > 0) {
+      return res.status(200).json({
+        status: true,
+      });
+    }
+
+    throw new Error('Activation key is invalid');
+  } catch (error) {
+    return res.status(400).json({
+      status: false,
+    });
+  }
+});
+
+app.listen(3030, () => console.log('Server ready on port 3030.'));
 
 module.exports = app;
